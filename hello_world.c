5d839c71226965598c954ad3135dde45cb952a9d
Pramit Gaha <pramitgaha2001@gmail.com>
2025-04-16 15:13:21 +0545

    hello world program

---
+++ hello_world.c
@@ -0,0 +1,11 @@
+#include "stdio.h"
+
+/*
+ * int main(void),
+ * here void means that main doesn't takes any arguments.
+ * NOTE: int main() != int main(void)
+ */
+int main(void) {
+  printf("Hello, world!"); // prints the message in the console
+  return 0; // indicates that the program exited successfully without any error
+}
