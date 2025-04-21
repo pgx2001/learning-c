482ea6e5934dac5731966680a436f15dd882b31a
Pramit Gaha <pramitgaha2001@gmail.com>
2025-04-21 19:09:14 +0545

    formatting fix

--- ch1/experimenting_escape_sequence.c
+++ ch1/experimenting_escape_sequence.c
@@ -1,16 +1,11 @@
 /*
  * TASK *
- * Experiment to find out what happens when printf's argument string contains
- \c,
- * where c is some character not listed above.
+ * Experiment to find out what happens when printf's argument string contains \c, where c is some character not listed above.
 
  * OUTCOME *
- * Compiling with command `gcc -o experimenting_escape_sequence
-experimenting_escape_sequence.c`
- results
- `experimenting_escape_sequence.c:10:26: warning: unknown escape sequence '\c'
-[-Wunknown-escape-s equence] 10 |   printf("Experimenting: \cat\n"); | ^~ 1
-warning generated.`
+ * Compiling with command `gcc -o experimenting_escape_sequence experimenting_escape_sequence.c`
+ results: 
+ `experimenting_escape_sequence.c:10:26: warning: unknown escape sequence '\c' [-Wunknown-escape-s equence] 10 |   printf("Experimenting:\cat\n"); | ^~ 1 warning generated.`
 * Running the compiled binary prints `cat`
  */
 
